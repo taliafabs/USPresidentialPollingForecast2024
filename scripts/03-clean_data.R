15c4525922f18effb4ff24b0008d5d5187f44021
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-10-30 12:09:49 -0400

    Clean the data and save it to data/02-analysis_data

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -14,21 +14,32 @@ library(haven)
 library(arrow)
 
 #### Clean data ####
-raw_data <- read_parquet("data/01-raw_data/raw_president_polls.parquet") |>
+raw_data <- 
+  read_parquet("data/01-raw_data/raw_data.parquet") |>
   clean_names() 
   
 cleaned_data <-
   raw_data |>
-  filter((candidate_name == "Kamala Harris" |
-           # candidate_name == "Joe Biden" |
-             candidate_name == "Donald Trump"), numeric_grade >= 2.7) |>
   mutate(
     state = if_else(is.na(state), "National", state),
     end_date = mdy(end_date),
     num = round((pct/100) * sample_size, 0)
     ) |>
-  filter(end_date >= as.Date("2024-07-21"))
+  filter(
+    (party== "DEM" | party == "REP"), 
+    numeric_grade >= 2.7,
+    end_date >= as.Date("2024-05-05")
+  )
 
+kamala <- 
+  cleaned_data |>
+  filter(candidate_name == "Kamala Harris", end_date >= as.Date("2024-07-21"))
+
+trump <- 
+  cleaned_data |>
+  filter(candidate_name == "Donald Trump", end_date >= as.Date("2024-07-21"))
 
 #### Save data ####
-write_parquet(cleaned_data, "data/02-analysis_data/presidential_polls_analysis_data.parquet")
+write_parquet(cleaned_data, "data/02-analysis_data/analysis_data.parquet")
+write_parquet(kamala, "data/02-analysis_data/kamala-analysis_data.parquet")
+write_parquet(trump, "data/02-analysis_data/trump-analysis_data.parquet")
