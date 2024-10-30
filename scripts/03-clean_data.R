a3bdbb70836ef124628f5ab5618985b87fbe1463
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-10-29 22:10:38 -0400

    clean the raw data from fivethirtyeight and save it to analysis data

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -15,20 +15,20 @@ library(arrow)
 
 #### Clean data ####
 raw_data <- read_parquet("data/01-raw_data/raw_president_polls.parquet") |>
-  clean_names()
-
-cleaned_data <- 
+  clean_names() 
+  
+cleaned_data <-
   raw_data |>
-  filter((candidate_name == "Kamala Harris" | 
-           candidate_name == "Joe Biden" | 
+  filter((candidate_name == "Kamala Harris" |
+           # candidate_name == "Joe Biden" |
              candidate_name == "Donald Trump"), numeric_grade >= 2.7) |>
   mutate(
-    state = if_else(is.na(state), "National", state), 
+    state = if_else(is.na(state), "National", state),
     end_date = mdy(end_date),
-    party = if_else(candidate_name=="Donald Trump", "Republican", "Democrat")
-  ) |>
-  filter(end_date >= as.Date("2024-05-05"))
+    num = round((pct/100) * sample_size, 0)
+    ) |>
+  filter(end_date >= as.Date("2024-07-21"))
 
 
 #### Save data ####
-write_csv(cleaned_data, "data/02-analysis_data.parquet")
+write_parquet(cleaned_data, "data/02-analysis_data/presidential_polls_analysis_data.parquet")
