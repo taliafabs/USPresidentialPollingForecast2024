68f02c699e0c20d9b6165479b9f09732e751c2fb
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-10-31 17:54:05 -0400

    final models

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -23,7 +23,7 @@ trump <- read_parquet("data/02-analysis_data/trump-analysis_data.parquet")
 
 # Bayesian model for pct_harris using spline and state and pollster as fixed effects
 harris_spline_model <- stan_glm(
-  pct_harris ~ ns(end_date_num, df = 5) + state + pollster,
+  pct_harris ~ ns(end_date_num, df = 5) + state + pollster + pollscore,
   data=harris,
   family = gaussian(),
   prior = normal(0, 5),
@@ -36,7 +36,7 @@ harris_spline_model <- stan_glm(
 
 # Bayesian model for pct_trump using spline and state and pollster as fixed effects
 trump_spline_model <- stan_glm(
-  pct_trump ~ ns(end_date_num, df = 5) + state + pollster, # Change df for the number of "bits" - higher numbers - more "wiggly" - but then need to worry about overfitting.
+  pct_trump ~ ns(end_date_num, df = 5) + state + pollster + pollscore, # Change df for the number of "bits" - higher numbers - more "wiggly" - but then need to worry about overfitting.
   data = trump,
   family = gaussian(),
   prior = normal(0, 5),
