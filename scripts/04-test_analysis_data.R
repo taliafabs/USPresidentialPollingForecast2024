0e413f68b9859212fe5f285516e31484c0e57db8
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-10-31 17:56:54 -0400

    analysis data tests so far

--- scripts/04-test_analysis_data.R
+++ scripts/04-test_analysis_data.R
@@ -1,69 +1,92 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 26 September 2024 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests the analysis data
+# Author: Talia Fabregas, Aliza Mithwani, Fatimah Yunusa
+# Date: 31 October 2024
+# Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites:
+# - The `tidyverse`, `arrow` and `testthat` package must be installed and loaded
+# - 02-download_data.R and 03-clean_data.R must have been run
+# Any other information needed? No
 
 
 #### Workspace setup ####
 library(tidyverse)
 library(testthat)
+library(arrow)
 
-data <- read_csv("data/02-analysis_data/analysis_data.csv")
-
+analysis_data <- read_parquet("data/02-analysis_data/analysis_data.parquet")
+harris_analysis_data <- read_parquet("data/02-analysis_data/harris-analysis_data.parquet")
+trump_analysis_data <- read_parquet("data/02-analysis_data/trump-analysis_data.parquet")
 
 #### Test data ####
-# Test that the dataset has 151 rows - there are 151 divisions in Australia
-test_that("dataset has 151 rows", {
-  expect_equal(nrow(analysis_data), 151)
+
+# Test that the analysis dataset has 310 rows
+test_that("analysis dataset has 310 rows", {
+  expect_equal(nrow(analysis_data), 310)
 })
 
-# Test that the dataset has 3 columns
-test_that("dataset has 3 columns", {
-  expect_equal(ncol(analysis_data), 3)
+# Test that the Harris analysis dataset and Trump analysis dataset both have 117 rows
+test_that("harris dataset has 117 rows", {
+  expect_equal(nrow(harris_analysis_data), 117)
 })
 
-# Test that the 'division' column is character type
-test_that("'division' is character", {
-  expect_type(analysis_data$division, "character")
+test_that("trump dataset has 117 rows", {
+  expect_equal(nrow(trump_analysis_data), 117)
 })
 
-# Test that the 'party' column is character type
-test_that("'party' is character", {
-  expect_type(analysis_data$party, "character")
+# Test that the analysis dataset has 10 columns
+test_that("analysis dataset has 10 columns", {
+  expect_equal(ncol(analysis_data), 10)
 })
 
-# Test that the 'state' column is character type
-test_that("'state' is character", {
-  expect_type(analysis_data$state, "character")
+# Test that the trump and harris analysis datasets have 9 columns each
+test_that("Harris analysis dataset has 9 columns", {
+  expect_equal(ncol(harris_analysis_data), 9)
 })
 
-# Test that there are no missing values in the dataset
-test_that("no missing values in dataset", {
-  expect_true(all(!is.na(analysis_data)))
+test_that("Trump analysis dataset has 9 columns", {
+  expect_equal(ncol(trump_analysis_data), 9)
 })
 
-# Test that 'division' contains unique values (no duplicates)
-test_that("'division' column contains unique values", {
-  expect_equal(length(unique(analysis_data$division)), 151)
+# Test that the 'candidate_name' column is character type
+test_that("'candidate_name' is character", {
+  expect_type(analysis_data$candidate_name, "character")
 })
 
-# Test that 'state' contains only valid Australian state or territory names
-valid_states <- c("New South Wales", "Victoria", "Queensland", "South Australia", "Western Australia", 
-                  "Tasmania", "Northern Territory", "Australian Capital Territory")
-test_that("'state' contains valid Australian state names", {
-  expect_true(all(analysis_data$state %in% valid_states))
-})
-
-# Test that there are no empty strings in 'division', 'party', or 'state' columns
-test_that("no empty strings in 'division', 'party', or 'state' columns", {
-  expect_false(any(analysis_data$division == "" | analysis_data$party == "" | analysis_data$state == ""))
-})
-
-# Test that the 'party' column contains at least 2 unique values
-test_that("'party' column contains at least 2 unique values", {
-  expect_true(length(unique(analysis_data$party)) >= 2)
-})
+# # Test that the 'party' column is character type
+# test_that("'party' is character", {
+#   expect_type(analysis_data$party, "character")
+# })
+# 
+# # Test that the 'state' column is character type
+# test_that("'state' is character", {
+#   expect_type(analysis_data$state, "character")
+# })
+# 
+# # Test that there are no missing values in the dataset
+# test_that("no missing values in dataset", {
+#   expect_true(all(!is.na(analysis_data)))
+# })
+# 
+# # Test that 'poll_id' contains 
+# test_that("'division' column contains unique values", {
+#   expect_equal(length(unique(analysis_data$division)), 151)
+# })
+# 
+# # Test that 'state' contains only National and included U.S. states
+# valid_states <- c("New South Wales", "Victoria", "Queensland", "South Australia", "Western Australia", 
+#                   "Tasmania", "Northern Territory", "Australian Capital Territory")
+# test_that("'state' contains valid Australian state names", {
+#   expect_true(all(analysis_data$state %in% valid_states))
+# })
+# 
+# # Test that there are no empty strings in 'division', 'party', or 'state' columns
+# test_that("no empty strings in 'division', 'party', or 'state' columns", {
+#   expect_false(any(analysis_data$division == "" | analysis_data$party == "" | analysis_data$state == ""))
+# })
+# 
+# # Test that the 'party' column contains at least 2 unique values
+# test_that("'party' column contains at least 2 unique values", {
+#   expect_true(length(unique(analysis_data$party)) >= 2)
+# })
