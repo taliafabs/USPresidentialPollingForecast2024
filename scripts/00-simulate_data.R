f1a1723d8b42c76e9229a502d4dac46529abf3e0
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-10-31 00:45:50 -0400

    simulate the data and save to data/00-simulated_data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -45,6 +45,14 @@ pollsters <- c("YouGov", "Siena/NYT", "CES / YouGov", "Marquette Law School",
 nominees <- c("Kamala Harris", "Donald Trump")
 dates <- seq(as.Date("2024-07-21"), as.Date("2024-10-29"), by = "day")
 
+# numeric grade for polls
+numeric_grades <- c(0.5, 0.6, 0.7, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 
+                   1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3)
+
+# sample sizes for polls
+sample_size <- round(rnorm(1, mean = 1700, sd = 500))
+sample_size <- pmax(500, pmin(sample_size, 10000))
+
 #### Simulate the Data ####
 simulated_data <- tibble(
   poll_id = 1:num_simulated_polls,
@@ -58,28 +66,29 @@ simulated_data <- tibble(
              0.075, 0.004, 0.19, 0.001, 0.001)
     ),
   pollster = sample(pollsters, num_simulated_polls, replace = TRUE),
+  sample_size = sample_size,
   # this makes support for Harris interact with the state. 
   # higher in states like Minnesota, Virginia, and New Hampshire
   # lower in Texas, Florida, Montana, Missouri
   # in the case where only trump and harris are considered, close to 50 in the
   # seven battleground states
   harris_pct = case_when(
-   state == "National" ~ rnorm(1, mean=50, sd=2.5),
-   state == "Michigan" ~ rnorm(1, mean=50, sd=2.5),
-   state == "Wisconsin" ~ rnorm(1, mean=50, sd=2.5),
-   state == "Pennsylvania" ~ rnorm(1, mean=50, sd=2.5),
-   state == "Arizona" ~ rnorm(1, mean=50, sd=2.5),
-   state == "Nevada" ~ rnorm(1, mean=50, sd=2.5),   
-   state == "Georgia" ~ rnorm(1, mean=50, sd=2.5),
-   state == "North Carolina" ~ rnorm(1, mean=50, sd=2.5),
-   state == "Florida" ~ rnorm(1, mean=43, sd=2.5),
-   state == "Texas" ~ rnorm(1, mean=45, sd=2.5),
-   state == "Nebraska" ~ rnorm(1, mean=0.38, sd=2.5),
-   state == "Nebraska CD-2" ~ rnorm(1, mean=0.53, sd=2.5),
-   state == "New Hampshire" ~ rnorm(1, mean=0.54, sd=2.5),
-   state == "Virgninia" ~ rnorm(1, mean=0.54, sd=2.5),
-   state == "Minnesota" ~ rnorm(1, mean=0.53, sd=2.5),
-   state == "Missouri" ~ rnorm(1, mean=0.41, sd=2.5)
+    state == "National" ~ pmax(0, pmin(rnorm(1, mean = 50, sd = 2.5), 100)),
+    state == "Michigan" ~ pmax(0, pmin(rnorm(1, mean = 50, sd = 2.5), 100)),
+    state == "Wisconsin" ~ pmax(0, pmin(rnorm(1, mean = 50, sd = 2.5), 100)),
+    state == "Pennsylvania" ~ pmax(0, pmin(rnorm(1, mean = 50, sd = 2.5), 100)),
+    state == "Arizona" ~ pmax(0, pmin(rnorm(1, mean = 50, sd = 2.5), 100)),
+    state == "Nevada" ~ pmax(0, pmin(rnorm(1, mean = 50, sd = 2.5), 100)),
+    state == "Georgia" ~ pmax(0, pmin(rnorm(1, mean = 50, sd = 2.5), 100)),
+    state == "North Carolina" ~ pmax(0, pmin(rnorm(1, mean = 50, sd = 2.5), 100)),
+    state == "Florida" ~ pmax(0, pmin(rnorm(1, mean = 44, sd = 2.5), 100)),
+    state == "Texas" ~ pmax(0, pmin(rnorm(1, mean = 45, sd = 2.5), 100)),
+    state == "Nebraska" ~ pmax(0, pmin(rnorm(1, mean = 38, sd = 2.5), 100)),
+    state == "Nebraska CD-2" ~ pmax(0, pmin(rnorm(1, mean = 53, sd = 2.5), 100)),
+    state == "New Hampshire" ~ pmax(0, pmin(rnorm(1, mean = 54, sd = 2.5), 100)),
+    state == "Virginia" ~ pmax(0, pmin(rnorm(1, mean = 54, sd = 2.5), 100)),
+    state == "Minnesota" ~ pmax(0, pmin(rnorm(1, mean = 53, sd = 2.5), 100)),
+    state == "Missouri" ~ pmax(0, pmin(rnorm(1, mean = 41, sd = 2.5), 100))
   ),
   trump_pct = 100-harris_pct
 )
