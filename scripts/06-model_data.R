f843159ed0640c2f17013209e9b31dc89b114adb
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-10-31 12:57:20 -0400

    final models

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -16,13 +16,15 @@ library(modelsummary)
 library(arrow)
 
 #### Read data ####
-# analysis_data <- read_parquet("data/02-analysis_data/analysis_data.parquet")
 harris <- read_parquet("data/02-analysis_data/harris-analysis_data.parquet")
 trump <- read_parquet("data/02-analysis_data/trump-analysis_data.parquet")
 
+#### Build models ####
+
+# Bayesian model for pct_harris using spline and state and pollster as fixed effects
 harris_spline_model <- stan_glm(
-  pct_harris ~ ns(end_date_num, df = 5) + state + pollster, # Change df for the number of "bits" - higher numbers - more "wiggly" - but then need to worry about overfitting.
-  data = harris,
+  pct_harris ~ ns(end_date_num, df = 5) + state + pollster,
+  data=harris,
   family = gaussian(),
   prior = normal(0, 5),
   prior_intercept = normal(50, 10),
@@ -32,6 +34,7 @@ harris_spline_model <- stan_glm(
   refresh = 0
 )
 
+# Bayesian model for pct_trump using spline and state and pollster as fixed effects
 trump_spline_model <- stan_glm(
   pct_trump ~ ns(end_date_num, df = 5) + state + pollster, # Change df for the number of "bits" - higher numbers - more "wiggly" - but then need to worry about overfitting.
   data = trump,
@@ -43,56 +46,6 @@ trump_spline_model <- stan_glm(
   chains = 4,
   refresh = 0
 )
-# model_data <- read_parquet("data/02-analysis_data/model_data.parquet")
-
-# model_data |>
-#   mutate(
-#   pollster = as.factor(pollster),
-#   state = as.factor(state),
-# )
-
-### Model data ####
-# first_model <-
-#   stan_glm(
-#     formula = flying_time ~ length + width,
-#     data = analysis_data,
-#     family = gaussian(),
-#     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-#     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-#     prior_aux = exponential(rate = 1, autoscale = TRUE),
-#     seed = 853
-#   )
-
-# harris |>
-#   mutate(
-#     pollster = factor(pollster),
-#     state = factor(state)
-#   )
-# 
-# trump |>
-#   mutate(
-#     pollster = factor(pollster),
-#     state = factor(state)
-#   )
-
-# end_date_num is the number of days since July 21, when Biden dropped out and endorsed Harris
-# harris_model <- lm(harris_avg_pct ~ end_date_num + pollster + state + sample_size, data=harris)
-# trump_model <- lm(trump_avg_pct ~ end_date_num + pollster + state + sample_size, data=trump)
-
-# priors <- normal(0, 2.5, autoscale = TRUE)
-# 
-# model_formula <- harris_won_poll ~ end_date_num + (1 | state) + (1 | pollster)
-# 
-# bayesian_model <- stan_glmer(
-#   formula = model_formula,
-#   data = model_data,
-#   family = binomial(link = "logit"),
-#   prior = priors,
-#   prior_intercept = priors,
-#   seed = 123,
-#   cores = 4,
-#   adapt_delta = 0.95
-# )
 
 #### Save model ####
 saveRDS(
@@ -104,19 +57,3 @@ saveRDS(
   trump_spline_model,
   file = "models/trump_model.rds"
 )
-
-# saveRDS(
-#   bayesian_model,
-#   file = "models/bayesian_model.rds"
-# )
-
-# saveRDS(
-#   harris_model,
-#   file = "models/harris_model.rds"
-# )
-# 
-# saveRDS(
-#   trump_model,
-#   file = "models/harris_model.rds"
-# )
-
