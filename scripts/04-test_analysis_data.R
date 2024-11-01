991011ca42492bf7ee88ae2ad0977193186e6511
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-11-01 19:56:07 -0400

    test the analysis data

--- scripts/04-test_analysis_data.R
+++ scripts/04-test_analysis_data.R
@@ -54,39 +54,6 @@ test_that("'candidate_name' is character", {
   expect_type(analysis_data$candidate_name, "character")
 })
 
-# # Test that the 'party' column is character type
-# test_that("'party' is character", {
-#   expect_type(analysis_data$party, "character")
-# })
-# 
-# # Test that the 'state' column is character type
-# test_that("'state' is character", {
-#   expect_type(analysis_data$state, "character")
-# })
-# 
-# # Test that there are no missing values in the dataset
-# test_that("no missing values in dataset", {
-#   expect_true(all(!is.na(analysis_data)))
-# })
-# 
-# # Test that 'poll_id' contains 
-# test_that("'division' column contains unique values", {
-#   expect_equal(length(unique(analysis_data$division)), 151)
-# })
-# 
-# # Test that 'state' contains only National and included U.S. states
-# valid_states <- c("New South Wales", "Victoria", "Queensland", "South Australia", "Western Australia", 
-#                   "Tasmania", "Northern Territory", "Australian Capital Territory")
-# test_that("'state' contains valid Australian state names", {
-#   expect_true(all(analysis_data$state %in% valid_states))
-# })
-# 
-# # Test that there are no empty strings in 'division', 'party', or 'state' columns
-# test_that("no empty strings in 'division', 'party', or 'state' columns", {
-#   expect_false(any(analysis_data$division == "" | analysis_data$party == "" | analysis_data$state == ""))
-# })
-# 
-# # Test that the 'party' column contains at least 2 unique values
-# test_that("'party' column contains at least 2 unique values", {
-#   expect_true(length(unique(analysis_data$party)) >= 2)
-# })
+test_that("'party' is character", {
+  expect_type(analysis_data$party, "character")
+})
