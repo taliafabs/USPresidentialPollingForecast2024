6954bd028619d3abee21ae13d762bb68bb23f827
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-11-01 19:54:01 -0400

    clean up the code

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -41,12 +41,6 @@ cleaned_data <-
     !is.na(pollscore)
   ) |>
   select(poll_id, pollster, numeric_grade, pollscore, methodology, state, end_date, population, party, candidate_name, pct, is_national)
-  # select(poll_id, pollster_id, pollster, numeric_grade, pollscore, methodology, 
-  #        transparency_score, state, end_date, sponsor_candidate_id, 
-  #        sponsor_candidate_party, endorsed_candidate_id, endorsed_candidate_party,
-  #        sample_size, population, internal, partisan, party, candidate_name, pct,
-  #        num, is_national
-  #        ) |>
 
 harris <- 
   cleaned_data |>
@@ -70,44 +64,6 @@ trump <-
   select(poll_id, pollster, numeric_grade, pollscore, methodology, state, end_date, population, num_trump, pct_trump, end_date_num)
   
 
-# # Subset the data to high-quality Harris and Trump polling estimates after she became presumptive DEM nominee
-# harris <- 
-#   cleaned_data |>
-#   filter(candidate_name == "Kamala Harris", end_date >= as.Date("2024-07-21")) |>
-#   mutate(
-#     num_harris = round((pct / 100) * sample_size, 0), # Need number not percent for some models
-#     pct_harris = pct
-#   ) |>
-#   group_by(poll_id, pollster_id, pollster, pollscore, transparency_score, end_date, 
-#            end_date_num, state, endorsed_candidate_party, sponsor_candidate_party) |>
-#   summarize(harris_avg_pct = mean(pct),
-#             harris_total = sum(num))
-# trump <- 
-#   cleaned_data |>
-#   filter(candidate_name == "Donald Trump", end_date >= as.Date("2024-07-21")) |>
-#   mutate(
-#     num_trump = round((pct / 100) * sample_size, 0), # Need number not percent for some models
-#     pct_trump = pct
-#   ) |>
-#   group_by(poll_id, pollster_id, pollster, pollscore, transparency_score, end_date, 
-#            end_date_num, state, endorsed_candidate_party, sponsor_candidate_party) |>
-#   summarize(trump_avg_pct = mean(pct),
-#          trump_total = sum(num))
-# 
-# 
-# # specifically choose to only keep the trump_avg_pct and trump_total from trump
-# # model data is derived from the analysis data; it just has the average support for Harris and Trump across each poll
-# # to determine who won the poll
-# model_data <- 
-#   harris |>
-#   left_join(
-#     select(trump, trump_avg_pct, trump_total),  # Select only poll_id and avg_pct from trump
-#     by = c("poll_id", "pollster_id", "pollster", "pollscore", "transparency_score", "end_date", 
-#            "end_date_num", "state", "endorsed_candidate_party")
-#   ) |>
-#   mutate(
-#     harris_won_poll = ifelse(harris_avg_pct > trump_avg_pct, 1, 0)
-#   )
 
 #### Save data ####
 write_parquet(cleaned_data, "data/02-analysis_data/analysis_data.parquet")
